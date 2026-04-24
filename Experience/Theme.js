45f5d450062e806d43ac2f58169d14a69beb507b
Aryan Gadhwal <gadhwalaryan1@gmail.com>
2026-04-25 02:31:21 +0530

    Deleted Experience directory

--- Experience/Theme.js
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from "events";
-
-export default class Theme extends EventEmitter {
-  constructor() {
-    super();
-    this.theme = "dark";
-    document.body.classList.add("dark-theme");
-  }
-}
